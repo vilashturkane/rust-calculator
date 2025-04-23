f88fc03035b7b7d18a3b0449e7862bfeab5483b3
mishalturkane <mishalturkane@gmail.com>
2025-04-23 22:57:24 +0000

    multiplication function added

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     println!("Addition is: {}", addition(10, 20));
     //bhai add  to ho gya ab substract kr de with branch
     println!("Substraction  is: {}", substract(20, 10));
+    println!("Multiplication  is: {}", multiply(20, 10));
 
 }
 
@@ -12,5 +13,10 @@ fn addition(a: i32, b: i32) -> i32 {
     a + b
 }
 fn substract(a: i32, b:i32) -> i32 {
-    a -b 
+    a - b 
 }
+
+fn multiply(a: i32, b:i32) -> i32 {
+    a * b 
+}
+
