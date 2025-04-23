1a36c813d381bdc2d4a0c0ccf8f8af972a12ad81
vilashturkane <vilashturkane@gmail.com>
2025-04-24 04:01:34 +0530

    bhai sbutract krna hai

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     println!("Mishal, tum addition ka code likho");
 
     println!("Addition is: {}", addition(10, 20));
+    //bhai add  to ho gya ab substract kr de with branch
 }
 
 fn addition(a: i32, b: i32) -> i32 {
